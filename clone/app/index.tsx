3d48cfe9ae4022c595a3a3bdb83f02cccd70336c
Vilas-cp <vilaspgowda1000@gmail.com>
2024-11-17 21:45:43 +0530

    tab done

--- clone/app/index.tsx
+++ clone/app/index.tsx
@@ -1,13 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import '../global.css'
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "expo-router";
 
 const index = () => {
-  return (
-    <View>
-      <Text className='text-pink-800'>index</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
 
-export default index
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
+
+  return (
+    <SafeAreaView >
+      <View className="">
+        <View className="flex-row items-center pb-3 space-x-2 mx-4 ">
+          <Image
+            source={{ uri: "https://links.papareact.com/wru" }}
+            className="w-7 h-7 bg-gray-300 rounded-full "
+          
+          />
+          <View className="ml-4 ">
+            <Text className="font-bold text-gray-500 text-xs">Deliver Now</Text>
+            <Text className="font-bold text-xl">Current Location</Text>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default index;
