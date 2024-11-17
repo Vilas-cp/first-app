2b7e3d12cacf54bbac3bf28f724db61b6404369a
Vilas-cp <vilaspgowda1000@gmail.com>
2024-11-17 19:31:14 +0530

    native wind added

--- clone/app/index.tsx
+++ clone/app/index.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import '../global.css'
 
 const index = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text className='text-pink-800'>index</Text>
     </View>
   )
 }
